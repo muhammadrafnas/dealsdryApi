0839a40dc331bd43b6d7a9f91a91f4f989fdf039
muhammadrafnas <rafnaszzz8164@gmail.com>
2022-05-11 10:22:13 +0530

    Email link added

--- backend/utils/zoopApi.js
+++ backend/utils/zoopApi.js
@@ -50,6 +50,7 @@ async function getPanDetailsfromApi(panNumber) {
             return { pannumber:panNumber, status:"Not valid" }
         } 
     } catch (error) {
+        console.log(error);
         return null
     }    
 }
